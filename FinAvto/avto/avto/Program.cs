0905a163c57b0a8b1a5c89b03ddee1f201c9a833
Mardjona <114858484+Mardjona@users.noreply.github.com>
2023-11-16 16:45:09 +0300

    Update Program.cs

--- FinAvto/avto/avto/Program.cs
+++ FinAvto/avto/avto/Program.cs
@@ -1,17 +1,30 @@
-﻿class Program
+using System.Collections.Generic;
+
+class Program
 {
-
-
     static void Main(string[] args)
     {
-        Avto avto = new Avto();
-        avto.EnterInfo();
-
-        avto.Move();
+      var avto1 = new Avto();
+        var avto2 = new Avto();
         
-        //avto.Zapravka();
+        List<Avto> avtos = new List<Avto>() {avto1, avto2};
+        //while (true)
+
+        while (true)
+        {
+            Console.WriteLine("Выберите машину: 1 или 2 ");
+            string g = Console.ReadLine();
+            if (g == "1")
+            {
+                avto1.Menu();
+            }
+            else if (g == "2")
+            {
+                avto2.Menu();
+            }
+        }
+        
     }
-}
 
 
 
